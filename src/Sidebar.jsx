9922438927eef08c110b96f6b44969f766514843
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-14 14:47:55 +0200

    Create Sidebar component, delete the default layout

---
+++ src/Sidebar.jsx
@@ -0,0 +1,19 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>CV creator</h1>
+      <ul>
+        <li>Personal data</li>
+        <li>Expierience</li>
+        <li>Education</li>
+        <li>Skills</li>
+        <li>Summary</li>
+        <li>Hobbies</li>
+        <li>Forgein languages</li>
+        <li>Certificates</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
