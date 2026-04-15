664b0ec270688b64de4c86798e399c339497e624
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-15 12:24:01 +0200

    Refactor the code, send props to section components

--- src/components/EditorPanel.jsx
+++ src/components/EditorPanel.jsx
@@ -5,19 +5,22 @@ import SummaryForm from "./features/SummaryForm.jsx";
 import HobbiesForm from "./features/HobbiesForm.jsx";
 
 const sectionComponents = {
-  personal: <PersonalForm />,
-  experience: <ExperienceForm />,
-  education: <EducationForm />,
-  summary: <SummaryForm />,
-  hobbies: <HobbiesForm />,
+  personal: PersonalForm,
+  experience: ExperienceForm,
+  education: EducationForm,
+  summary: SummaryForm,
+  hobbies: HobbiesForm,
 };
 
-export default function EditorPanel({ activeSection }) {
-  return (
-    sectionComponents[activeSection] || (
+export default function EditorPanel({ activeSection, cvData, setCvData }) {
+  const ActiveComponent = sectionComponents[activeSection];
+
+  if (!ActiveComponent)
+    return (
       <div>
         <p>Select a section</p>
       </div>
-    )
-  );
+    );
+
+  return <ActiveComponent cvData={cvData} setCvData={setCvData} />;
 }
