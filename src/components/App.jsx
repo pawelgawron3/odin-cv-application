11804875f19ae752b88eecf06e210dd172d7e3ea
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-15 12:22:28 +0200

    Add new state variable, update the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
 import Sidebar from "./Sidebar.jsx";
 import EditorPanel from "./EditorPanel.jsx";
+import Preview from "./Preview.jsx";
 
 export default function App() {
   const [activeSection, setActiveSection] = useState("personal");
+  const [cvData, setCvData] = useState({
+    personal: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      github: "",
+      phoneNumber: "",
+      address: "",
+    },
+    experience: {
+      jobName: "",
+      companyName: "",
+      city: "",
+      startDate: "",
+      endDate: "",
+      jobSummary: "",
+    },
+    education: {
+      schoolName: "",
+      degree: "",
+      city: "",
+      startDate: "",
+      endDate: "",
+    },
+    summary: "",
+    hobbies: "",
+  });
 
   return (
     <div className="app">
@@ -11,7 +39,12 @@ export default function App() {
         activeSection={activeSection}
         setActiveSection={setActiveSection}
       />
-      <EditorPanel activeSection={activeSection} />
+      <EditorPanel
+        activeSection={activeSection}
+        cvData={cvData}
+        setCvData={setCvData}
+      />
+      <Preview cvData={cvData} />
     </div>
   );
 }
